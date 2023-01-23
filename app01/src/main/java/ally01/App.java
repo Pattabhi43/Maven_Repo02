6303a4efd89f5c2f3a4c6b8d4db3a9d1c0d7a371
Pattabhi43 <kpramaiah46@gmail.com>
2023-01-23 21:50:28 +0530

    created new app01 files

---
+++ app01/src/main/java/ally01/App.java
@@ -0,0 +1,14 @@
+package ally01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My name is Pattabhi" );
+        System.out.println( "My name is PRK" );
+    }
+}
